07ba09f2643494b68bfdf04e1c05a0706a09a5d4
Leo <liyung.chen@tum.de>
2025-01-24 17:17:17 +0100

    eslint lint-staged

--- lib/app-stage.ts
+++ lib/app-stage.ts
@@ -1,41 +1,37 @@
-import { EcsFargateStack } from '../stacks/EcsFargateStack';
-import { Stage, StageProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import { EcsFargateStack } from "../stacks/EcsFargateStack";
+import { Stage, StageProps } from "aws-cdk-lib";
+import { Construct } from "constructs";
 // import { ApiGatewayCustomDomainStack } from '../stacks/ApiGatewayCustomDomainStack';
 // import { AuthStack } from "./authstack";
 
 interface DeploymentProps extends StageProps {
-  stageName: string;
-  domainStage: string;
-  isProd: boolean;
-  secretArn: string;
+    stageName: string;
+    domainStage: string;
+    isProd: boolean;
+    secretArn: string;
 }
 
 export class PipelineAppStage extends Stage {
-  constructor(scope: Construct, id: string, props: DeploymentProps) {
-    super(scope, id, props);
+    constructor(scope: Construct, id: string, props: DeploymentProps) {
+        super(scope, id, props);
 
-    new EcsFargateStack(
-      this,
-      `EcsFargateStack-${props.stageName}`,
-      {
-        env: props.env,
-        stageName: props.stageName,
-        domainStage: props.domainStage,
-        isProd: props.isProd,
-        secretArn: props.secretArn,
-      }
-    );
+        new EcsFargateStack(this, `EcsFargateStack-${props.stageName}`, {
+            env: props.env,
+            stageName: props.stageName,
+            domainStage: props.domainStage,
+            isProd: props.isProd,
+            secretArn: props.secretArn
+        });
 
-    // new ApiGatewayCustomDomainStack(
-    //   this,
-    //   `ApiGatewayCustomDomainStack-${props.stageName}`,
-    //   {
-    //     env: props.env,
-    //     stageName: props.stageName,
-    //     domainStage: props.domainStage,
-    //     isProd: props.isProd,
-    //   }
-    // );
-  }
+        // new ApiGatewayCustomDomainStack(
+        //   this,
+        //   `ApiGatewayCustomDomainStack-${props.stageName}`,
+        //   {
+        //     env: props.env,
+        //     stageName: props.stageName,
+        //     domainStage: props.domainStage,
+        //     isProd: props.isProd,
+        //   }
+        // );
+    }
 }
