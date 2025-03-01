0e010a0a5a70bb8ec37375d4a1cbe3427e891240
Leo <liyung.chen@tum.de>
2025-03-01 13:12:27 +0100

    update file name

--- bin/tai_ger_portal_service.ts
+++ bin/tai_ger_portal_service.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import * as cdk from "aws-cdk-lib";
-import { TaiGerPortalServicePipelineStack } from "../lib/tai_ger_portal_service-stack";
+import { TaiGerPortalServicePipelineStack } from "../lib/tai_ger_portal_service_pipelinestack";
 import { AWS_ACCOUNT } from "../configuration";
 import { Region } from "../constants";
 
